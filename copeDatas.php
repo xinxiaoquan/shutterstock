c1694bf0f6cfd8da55ccf90f75cc833b219eb01f
耿康康 <3111381465@qq.com>
2021-05-23 21:47:12 +0800

    2021-5-23

---
+++ copeDatas.php
@@ -0,0 +1,65 @@
+<?php
+ini_set("extension","gd");
+//ini_set("extension","exif");
+
+$config=json_decode(
+	file_get_contents("config.json")
+);
+$index=file_get_contents("index");
+$dirDatas=json_decode(
+	file_get_contents("dirDatas.json")
+);
+$index=(int)$index;
+if(count($dirDatas)<=$index)
+	exit;
+$path=$dirDatas[$index];
+$info=getimagesize($path);
+$width=$info[0];
+$height=$info[1];
+$type=$info["mime"];
+$suffix=str_replace("image/", "", $type);
+if($suffix=="png") {
+	$img=imagecreatefrompng($path);
+	$path=$config->inputImages.$index."jpeg";
+	$suffix="jpeg";
+	imagejpeg($img, $path);
+}
+if($suffix && $width*$height>=4000000) {
+	copy($path, $config->outputImages.$index.".".$suffix);
+	echo "{$path} -- 通过(pass)";
+}
+echo "_";
+
+file_put_contents("index", $index+1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ createDirDatas.php
@@ -0,0 +1,54 @@
+<?php
+$config=json_decode(
+	file_get_contents("config.json")
+);
+global $pathArr;
+$pathArr=array();
+
+getDirInfo($config->inputImages);
+
+file_put_contents("dirDatas.json",json_encode($pathArr));
+file_put_contents("index", "0");
+
+echo count($pathArr);
+
+function getDirInfo($path) {
+	$dir=opendir($path);
+	while($fileName=readdir($dir)) {
+		if($fileName=="." || $fileName=="..")
+			continue;
+		if(is_dir($path.$fileName))
+			getDirInfo($path.$fileName."/");
+		else $GLOBALS["pathArr"][]=$path.$fileName;
+	}
+	closedir($dir);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
